dc494812ec63d9f208b57c3658972c409af87cf2
Stephane Denomme <stephane.denomme@virtuso.ca>
2017-03-20 14:17:05 -0400

    test groovy

--- Groovy/src/Jenkins.groovy
+++ Groovy/src/Jenkins.groovy
@@ -3,15 +3,11 @@ node('')
 {
     stage('Checkout')
     {
-        echo( 'Hello $$$ world!')
         git credentialsId: '289766ef-e84f-4e38-91f6-4984b6dd6594', url: 'https://github.com/CurlyFire/TestPipeline.git'
     }
-    stage('Set file versions')
+    stage('Set project.json files version')
     {
-        def a = 'for f in $(find -name \'project.json\'); do cat $f | jq --arg v ${version} \'.version = $v\' > temp.json && mv temp.json $f ; done'
-        echo ('Ok!')
-        echo(a)
-        sh(a)
+        sh('for f in $(find -name \'project.json\'); do cat $f | jq --arg v ${version} \'.version = $v\' > temp.json && mv temp.json $f ; done')
     }
 }
 
