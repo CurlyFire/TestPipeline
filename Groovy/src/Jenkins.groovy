e5fb1aad406f5e6be842cbe94b43cfd3d2b57e73
Stephane Denomme <stephane.denomme@virtuso.ca>
2017-03-20 13:46:20 -0400

    test groovy

--- Groovy/src/Jenkins.groovy
+++ Groovy/src/Jenkins.groovy
@@ -9,6 +9,7 @@ node('')
     stage('Set file versions')
     {
         def a = 'for f in $(find -name \'project.json\'); do cat $f | jq --arg v ${version} \'.version = $v\' > temp.json && mv temp.json $f ; done'
+        echo ('Ok!')
         echo(a)
         sh(a)
     }
