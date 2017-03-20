1c2113c4bd0dea12967bdef6a8119ff91d7e866e
Stephane Denomme <stephane.denomme@virtuso.ca>
2017-03-20 14:29:35 -0400

    test groovy

--- Groovy/src/Jenkins.groovy
+++ Groovy/src/Jenkins.groovy
@@ -5,9 +5,21 @@ node('')
     {
         git credentialsId: '289766ef-e84f-4e38-91f6-4984b6dd6594', url: 'https://github.com/CurlyFire/TestPipeline.git'
     }
-    stage('Set project.json files version')
+
+    stage('Set version')
     {
         sh('for f in $(find -name \'project.json\'); do cat $f | jq --arg v ${version} \'.version = $v\' > temp.json && mv temp.json $f ; done')
     }
+
+    stage('Restore')
+    {
+        sh('dotnet restore --configfile ~/.config/NuGet/NuGet.Config')
+    }
+
+    stage('Restore')
+    {
+        sh('dotnet build **/project.json')
+    }
+
 }
 
