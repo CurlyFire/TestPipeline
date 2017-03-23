86985265316f02ba37898e1982dab890e205552c
Stephane Denomme <stephane.denomme@virtuso.ca>
2017-03-23 15:53:09 -0400

    test groovy

--- Groovy/src/Jenkins.groovy
+++ Groovy/src/Jenkins.groovy
@@ -1,10 +1,10 @@
 #!groovy
 node('')
 {
-    //stage('Checkout')
-    //{
-    //    git credentialsId: '289766ef-e84f-4e38-91f6-4984b6dd6594', url: 'https://github.com/CurlyFire/TestPipeline.git'
-    //}
+    stage('Checkout')
+    {
+        git credentialsId: '289766ef-e84f-4e38-91f6-4984b6dd6594', url: 'https://github.com/CurlyFire/TestPipeline.git'
+    }
 
     stage('Set version')
     {
